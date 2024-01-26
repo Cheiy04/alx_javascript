a2a4b4b494e66c67dc6a0fa084ff5b9866704059
Adrian Chege <achege116@gmail.com>
2024-01-27 01:30:29 +0300

    Three outputs

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+// Script that prints three outputs
+
+let firstChar = "C is fun";
+let secondChar = "Python is cool";
+let thirdChar = "JavaScript is amazing";
+
+console.log(firstChar);
+console.log(secondChar);
+console.log(thirdChar);
