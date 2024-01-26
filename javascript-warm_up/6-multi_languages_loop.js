6a4d1abb33753a1d2f999ba4d4491bd6e330bec3
Adrian Chege <achege116@gmail.com>
2024-01-27 01:49:21 +0300

    loop to language

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// script that returns three outputs using a loop
+
+let lang =["C is fun", "Python is cool", "JavaScript is amazing"]
+
+for  (let i =0; i< lang.length; i++){
+    console.log(lang[i]);
+}
