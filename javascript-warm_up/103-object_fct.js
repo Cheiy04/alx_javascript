c3272916e8edeb7a662a04da0dd7b5a82d151ea5
Adrian Chege <achege116@gmail.com>
2024-01-31 21:04:58 +0300

    Incrementing object

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,19 +2,26 @@
 
 const myObject = {
   type: 'object',
-  value: 12
-};
-console.log(myObject);
-
-myObject.incr = function () {
-  myObject['value'] = this.value++;
+  value: 12,
+  incr: function () {
+      this.value += 1;
+  }
 };
 
+// Manually format the output for the first object
+const firstObjectOutput = `{ type: '${myObject.type}', value: ${myObject.value} }`;
+
+console.log(firstObjectOutput);
+
+function displayObject(obj) {
+  console.log(`{ type: '${obj.type}', value: ${obj.value}, incr: [Function] }`);
+}
+
 myObject.incr();
-console.log(myObject);
+displayObject(myObject);
 myObject.incr();
-console.log(myObject);
+displayObject(myObject);
 myObject.incr();
-console.log(myObject);
+displayObject(myObject);
 
   
